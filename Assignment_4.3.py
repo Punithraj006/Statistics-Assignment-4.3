a316e98780db3977db229ba0c15f176f3692d79b
Punithraj006 <39230591+Punithraj006@users.noreply.github.com>
2018-06-13 11:54:28 +0530

    Statistics Assignment 4.3

---
+++ Assignment_4.3.py
@@ -0,0 +1,15 @@
+Problem Statement: 4.3
+
+In a class on 100 students, 80 students passed in all subjects, 10 failed in one subject, 7 failed in two 
+subjects and 3 failed in three subjects. Find the probability distribution (PD) of
+the variable for number of subjects a student from the given class has failed in.
+
+
+The PD of failing in 0 subjects, P(X=0) = 0.8
+
+The PD of failing in 1 subjects, P(X=1) = 0.1
+
+The PD of failing in 2 subjects, P(X=2) = 0.07
+
+The PD of failing in 3 subjects, P(X=3) = 0.03
+
